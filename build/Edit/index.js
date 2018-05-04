bfd3423e4ed56afac1cc708524d814602198e34b
huangjl <huangjl@101.com>
2018-05-04 19:50:38 +0800

    add

--- build/Edit/index.js
+++ build/Edit/index.js
@@ -1,1 +1,348 @@
-!function(){var t=function(){return function(t){function e(o){if(n[o])return n[o].exports;var a=n[o]={exports:{},id:o,loaded:!1};return t[o].call(a.exports,a,a.exports,e),a.loaded=!0,a.exports}var n={};return e.m=t,e.c=n,e.p="",e(0)}([function(t,e,n){var o=n(4),a=n(5),i=n(6);$app_define$("@app-component/index",[],function(t,e,n){i(n,e,t),e.__esModule&&e.default&&(n.exports=e.default),n.exports.template=o,n.exports.style=a}),$app_bootstrap$("@app-component/index",{packagerVersion:"0.0.5"})},,,,function(t,e){t.exports={type:"div",attr:{},classList:["layout"],children:[{type:"div",attr:{},classList:["title"],children:[{type:"input",attr:{type:"text",placeholder:"标题",value:function(){return this.title}},events:{change:"handleTitleChange"}}]},{type:"div",attr:{},classList:["content"],children:[{type:"textarea",attr:{placeholder:"内容",value:function(){return this.content}},events:{change:"handleContentChange"}}]},{type:"div",attr:{},classList:["footer"],children:[{type:"input",attr:{type:"button",value:"保存"},classList:["btn","btn-success"],events:{click:"handleSaveClick"}},{type:"input",attr:{type:"button",value:"返回"},classList:["btn","btn-cancel"],events:{click:"handleCancelClick"}}]}]}},function(t,e){t.exports={".layout":{flexDirection:"column"},".title":{height:"10%",paddingTop:"0px",paddingRight:"20px",paddingBottom:"0px",paddingLeft:"20px"},".title input":{width:"100%",color:"#191919",borderBottomWidth:"1px",borderBottomColor:"#e6e6e6",_meta:{ruleDef:[{t:"a",n:"class",i:!1,a:"element",v:"title"},{t:"d"},{t:"t",n:"input"}]}},".content":{height:"92%",paddingTop:"20px",paddingRight:"20px",paddingBottom:"20px",paddingLeft:"20px"},".content textarea":{width:"100%",height:"100%",color:"#636363",_meta:{ruleDef:[{t:"a",n:"class",i:!1,a:"element",v:"content"},{t:"d"},{t:"t",n:"textarea"}]}},".footer":{height:"8%"},".btn":{flex:1,color:"#ffffff"},".btn-success":{backgroundColor:"#519650"},".btn-cancel":{backgroundColor:"#FFA500"}}},function(t,e){t.exports=function(t,e,n){"use strict";function o(t){return t&&t.__esModule?t:{default:t}}Object.defineProperty(e,"__esModule",{value:!0});var a="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t},i=n("@app-module/system.router"),r=o(i);e.default={data:{title:"",content:"",index:""},onShow:function(){if(this.index){var t=this.$app.$def.data.items[this.index],e=t.title,n=t.content;this.title=e,this.content=n}},onMenuPress:function(){this.$app.showMenu()},handleTitleChange:function(t){this.title=t.value},handleContentChange:function(t){this.content=t.value},handleSaveClick:function(t){var e=this.title,n=this.content,o=this.$app.$def.data.items,a={title:e,content:n,time:+new Date};this.index?o.splice(+this.index,1,a):o.push(a),this.$app.saveData(this.handleCancelClick)},handleCancelClick:function(t){r.default.back()}};var c=e.default||t.exports,l=["public","protected","private"];if(c.data&&l.some(function(t){return c[t]}))throw new Error('页面VM对象中的属性data不可与"'+l.join(",")+'"同时存在，请使用private替换data名称');c.data||(c.data={},c._descriptor={},l.forEach(function(t){var e=a(c[t]);if("object"===e){c.data=Object.assign(c.data,c[t]);for(var n in c[t])c._descriptor[n]={access:t}}else"function"===e&&console.warn("页面VM对象中的属性"+t+"的值不能是函数，请使用对象")}))}}])};return"undefined"==typeof window?t():void(window.createPageHandler=t)}();
+(function(){
+      
+  var createPageHandler = function() {
+    return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $app_template$ = __webpack_require__(4)
+	var $app_style$ = __webpack_require__(5)
+	var $app_script$ = __webpack_require__(6)
+	
+	$app_define$('@app-component/index', [], function($app_require$, $app_exports$, $app_module$){
+	     $app_script$($app_module$, $app_exports$, $app_require$)
+	     if ($app_exports$.__esModule && $app_exports$.default) {
+	            $app_module$.exports = $app_exports$.default
+	        }
+	     $app_module$.exports.template = $app_template$
+	     $app_module$.exports.style = $app_style$
+	})
+	
+	$app_bootstrap$('@app-component/index',{ packagerVersion: '0.0.5'})
+
+
+/***/ },
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = {
+	  "type": "div",
+	  "attr": {},
+	  "classList": [
+	    "layout"
+	  ],
+	  "children": [
+	    {
+	      "type": "div",
+	      "attr": {},
+	      "classList": [
+	        "title"
+	      ],
+	      "children": [
+	        {
+	          "type": "input",
+	          "attr": {
+	            "type": "text",
+	            "placeholder": "标题",
+	            "value": function () {return this.title}
+	          },
+	          "events": {
+	            "change": "handleTitleChange"
+	          }
+	        }
+	      ]
+	    },
+	    {
+	      "type": "div",
+	      "attr": {},
+	      "classList": [
+	        "content"
+	      ],
+	      "children": [
+	        {
+	          "type": "textarea",
+	          "attr": {
+	            "placeholder": "内容",
+	            "value": function () {return this.content}
+	          },
+	          "events": {
+	            "change": "handleContentChange"
+	          }
+	        }
+	      ]
+	    },
+	    {
+	      "type": "div",
+	      "attr": {},
+	      "classList": [
+	        "footer"
+	      ],
+	      "children": [
+	        {
+	          "type": "input",
+	          "attr": {
+	            "type": "button",
+	            "value": "保存"
+	          },
+	          "classList": [
+	            "btn",
+	            "btn-success"
+	          ],
+	          "events": {
+	            "click": "handleSaveClick"
+	          }
+	        },
+	        {
+	          "type": "input",
+	          "attr": {
+	            "type": "button",
+	            "value": "返回"
+	          },
+	          "classList": [
+	            "btn",
+	            "btn-cancel"
+	          ],
+	          "events": {
+	            "click": "handleCancelClick"
+	          }
+	        }
+	      ]
+	    }
+	  ]
+	}
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	module.exports = {
+	  ".layout": {
+	    "flexDirection": "column"
+	  },
+	  ".title": {
+	    "height": "10%",
+	    "paddingTop": "0px",
+	    "paddingRight": "20px",
+	    "paddingBottom": "0px",
+	    "paddingLeft": "20px"
+	  },
+	  ".title input": {
+	    "width": "100%",
+	    "color": "#191919",
+	    "borderBottomWidth": "1px",
+	    "borderBottomColor": "#e6e6e6",
+	    "_meta": {
+	      "ruleDef": [
+	        {
+	          "t": "a",
+	          "n": "class",
+	          "i": false,
+	          "a": "element",
+	          "v": "title"
+	        },
+	        {
+	          "t": "d"
+	        },
+	        {
+	          "t": "t",
+	          "n": "input"
+	        }
+	      ]
+	    }
+	  },
+	  ".content": {
+	    "height": "92%",
+	    "paddingTop": "20px",
+	    "paddingRight": "20px",
+	    "paddingBottom": "20px",
+	    "paddingLeft": "20px"
+	  },
+	  ".content textarea": {
+	    "width": "100%",
+	    "height": "100%",
+	    "color": "#636363",
+	    "_meta": {
+	      "ruleDef": [
+	        {
+	          "t": "a",
+	          "n": "class",
+	          "i": false,
+	          "a": "element",
+	          "v": "content"
+	        },
+	        {
+	          "t": "d"
+	        },
+	        {
+	          "t": "t",
+	          "n": "textarea"
+	        }
+	      ]
+	    }
+	  },
+	  ".footer": {
+	    "height": "8%"
+	  },
+	  ".btn": {
+	    "flex": 1,
+	    "color": "#ffffff"
+	  },
+	  ".btn-success": {
+	    "backgroundColor": "#519650"
+	  },
+	  ".btn-cancel": {
+	    "backgroundColor": "#FFA500"
+	  }
+	}
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	module.exports = function(module, exports, $app_require$){'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+	
+	var _system = $app_require$('@app-module/system.router');
+	
+	var _system2 = _interopRequireDefault(_system);
+	
+	var _system3 = $app_require$('@app-module/system.prompt');
+	
+	var _system4 = _interopRequireDefault(_system3);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	exports.default = {
+	  public: {
+	    index: ''
+	  },
+	  private: {
+	    title: '',
+	    content: ''
+	  },
+	  onShow: function onShow() {
+	    if (this.index) {
+	      var _$app$$def$data$items = this.$app.$def.data.items[this.index],
+	          title = _$app$$def$data$items.title,
+	          content = _$app$$def$data$items.content;
+	
+	      this.title = title;
+	      this.content = content;
+	    }
+	  },
+	  onMenuPress: function onMenuPress() {
+	    this.$app.showMenu();
+	  },
+	  handleTitleChange: function handleTitleChange(e) {
+	    this.title = e.value;
+	  },
+	  handleContentChange: function handleContentChange(e) {
+	    this.content = e.value;
+	  },
+	  handleSaveClick: function handleSaveClick(e) {
+	    var title = this.title,
+	        content = this.content,
+	        items = this.$app.$def.data.items;
+	
+	    var data = {
+	      title: title,
+	      content: content,
+	      time: +new Date()
+	    };
+	    if (this.index) {
+	      items.splice(+this.index, 1, data);
+	    } else {
+	      items.push(data);
+	    }
+	    this.$app.saveData(this.handleCancelClick);
+	  },
+	  handleCancelClick: function handleCancelClick(e) {
+	    _system2.default.back();
+	  }
+	};
+	
+	
+	var moduleOwn = exports.default || module.exports;
+	var accessors = ['public', 'protected', 'private'];
+	
+	if (moduleOwn.data && accessors.some(function (acc) {
+	  return moduleOwn[acc];
+	})) {
+	  throw new Error('页面VM对象中的属性data不可与"' + accessors.join(',') + '"同时存在，请使用private替换data名称');
+	} else if (!moduleOwn.data) {
+	  moduleOwn.data = {};
+	  moduleOwn._descriptor = {};
+	  accessors.forEach(function (acc) {
+	    var accType = _typeof(moduleOwn[acc]);
+	    if (accType === 'object') {
+	      moduleOwn.data = Object.assign(moduleOwn.data, moduleOwn[acc]);
+	      for (var name in moduleOwn[acc]) {
+	        moduleOwn._descriptor[name] = { access: acc };
+	      }
+	    } else if (accType === 'function') {
+	      console.warn('页面VM对象中的属性' + acc + '的值不能是函数，请使用对象');
+	    }
+	  });
+	}}
+
+/***/ }
+/******/ ]);
+  };
+  if (typeof window === "undefined") {
+    return createPageHandler();
+  }
+  else {
+    window.createPageHandler = createPageHandler
+  }
+})();
+//# sourceMappingURL=index.js.map
--- build/app.js
+++ build/app.js
@@ -1,1 +1,303 @@
-!function(){var e=function(){return function(e){function o(n){if(t[n])return t[n].exports;var a=t[n]={exports:{},id:n,loaded:!1};return e[n].call(a.exports,a,a.exports,o),a.loaded=!0,a.exports}var t={};return o.m=e,o.c=t,o.p="",o(0)}([function(e,o,t){var n=t(10);$app_define$("@app-application/app",[],function(e,o,t){n(t,o,e),o.__esModule&&o.default&&(t.exports=o.default)}),$app_bootstrap$("@app-application/app",{packagerVersion:"0.0.5"})},,,,,,,,,,function(e,o,t){e.exports=function(e,o,n){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(o,"__esModule",{value:!0});var s=t(11),i=a(s),r=n("@app-module/system.storage"),u=a(r),c=t(14),p=a(c);o.default={showMenu:p.default.showMenu,createShortcut:p.default.createShortcut,data:{items:[]},deleteData:function(){u.default.delete({key:"app_data",success:function(e){console.log("handling success")},fail:function(e,o){console.log("handling fail, code="+o)}})},saveData:function(e){u.default.set({key:"app_data",value:(0,i.default)(this.$def.data.items),success:function(o){console.log("handling success"),e()},fail:function(e,o){console.log("handling fail, code="+o)}})},onCreate:function(){var e=this;console.info("Application onCreate"),u.default.get({key:"app_data",success:function(o){console.log("handling success"),o&&(e.$def.data.items=JSON.parse(o))},fail:function(e,o){console.log("handling fail, code="+o)}})},onDestroy:function(){console.info("Application onDestroy")}},(o.default||e.exports).manifest={package:"com.quick.notebook.plus",name:"快记事",versionName:"1.0.0",versionCode:"2",minPlatformVersion:"101",icon:"/Common/officialmark.png",features:[{name:"system.prompt"},{name:"system.router"},{name:"system.shortcut"},{name:"system.storage"}],permissions:[{origin:"*"}],config:{logLevel:"log"},router:{entry:"Home",pages:{Home:{component:"index"},Edit:{component:"index"},About:{component:"index"}}},display:{titleBarBackgroundColor:"#4d2517",titleBarTextColor:"#ad9a94",menu:!0,titleBarText:"快记事",pages:{Home:{},Edit:{},About:{menu:!1}}}}}},function(e,o,t){e.exports={default:t(12),__esModule:!0}},function(e,o,t){var n=t(13),a=n.JSON||(n.JSON={stringify:JSON.stringify});e.exports=function(e){return a.stringify.apply(a,arguments)}},function(e,o){var t=e.exports={version:"2.5.3"};"number"==typeof __e&&(__e=t)},function(e,o){"use strict";function t(){var e=$app_require$("@app-module/system.prompt"),o=$app_require$("@app-module/system.router"),t=$app_require$("@app-module/system.app").getInfo();e.showContextMenu({itemList:["保存桌面","关于","取消"],success:function(a){switch(a.index){case 0:n();break;case 1:o.push({uri:"/About",params:{name:t.name,icon:t.icon}});break;case 2:break;default:e.showToast({message:"error"})}}})}function n(){var e=$app_require$("@app-module/system.prompt"),o=$app_require$("@app-module/system.shortcut");o.hasInstalled({success:function(t){t?e.showToast({message:"已创建桌面图标"}):o.install({success:function(){e.showToast({message:"成功创建桌面图标"})},fail:function(o,t){e.showToast({message:"error: "+t+"---"+o})}})}})}Object.defineProperty(o,"__esModule",{value:!0}),o.default={showMenu:t,createShortcut:n}}])};return"undefined"==typeof window?e():(window.createAppHandler=e,void(global.manifest={package:"com.quick.notebook.plus",name:"快记事",versionName:"1.0.0",versionCode:"2",minPlatformVersion:"101",icon:"/Common/officialmark.png",features:[{name:"system.prompt"},{name:"system.router"},{name:"system.shortcut"},{name:"system.storage"}],permissions:[{origin:"*"}],config:{logLevel:"log"},router:{entry:"Home",pages:{Home:{component:"index"},Edit:{component:"index"},About:{component:"index"}}},display:{titleBarBackgroundColor:"#4d2517",titleBarTextColor:"#ad9a94",menu:!0,titleBarText:"快记事",pages:{Home:{},Edit:{},About:{menu:!1}}}}))}();
+(function(){
+      
+  var createAppHandler = function() {
+    return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $app_script$ = __webpack_require__(10)
+	
+	$app_define$('@app-application/app', [], function($app_require$, $app_exports$, $app_module$){
+	     $app_script$($app_module$, $app_exports$, $app_require$)
+	     if ($app_exports$.__esModule && $app_exports$.default) {
+	            $app_module$.exports = $app_exports$.default
+	        }
+	})
+	
+	$app_bootstrap$('@app-application/app',{ packagerVersion: '0.0.5'})
+
+
+/***/ },
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */,
+/* 5 */,
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */,
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = function(module, exports, $app_require$){'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _stringify = __webpack_require__(11);
+	
+	var _stringify2 = _interopRequireDefault(_stringify);
+	
+	var _system = $app_require$('@app-module/system.storage');
+	
+	var _system2 = _interopRequireDefault(_system);
+	
+	var _util = __webpack_require__(14);
+	
+	var _util2 = _interopRequireDefault(_util);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	exports.default = {
+	  showMenu: _util2.default.showMenu,
+	  createShortcut: _util2.default.createShortcut,
+	  data: {
+	    items: []
+	  },
+	  deleteDataByIndex: function deleteDataByIndex(index) {
+	    this.data.items.splice(index, 1);
+	    _system2.default.set({
+	      key: 'app_data',
+	      value: (0, _stringify2.default)(this.$def.data.items),
+	      success: function (_success) {
+	        function success(_x) {
+	          return _success.apply(this, arguments);
+	        }
+	
+	        success.toString = function () {
+	          return _success.toString();
+	        };
+	
+	        return success;
+	      }(function (data) {
+	        console.log("handling success");
+	        success();
+	      }),
+	      fail: function fail(data, code) {
+	        console.log("handling fail, code=" + code);
+	      }
+	    });
+	  },
+	  deleteAllData: function deleteAllData() {
+	    this.data.items = [];
+	    _system2.default.delete({
+	      key: 'app_data',
+	      success: function success(data) {
+	        console.log("handling success");
+	      },
+	      fail: function fail(data, code) {
+	        console.log("handling fail, code=" + code);
+	      }
+	    });
+	  },
+	  saveData: function saveData(success) {
+	    _system2.default.set({
+	      key: 'app_data',
+	      value: (0, _stringify2.default)(this.$def.data.items),
+	      success: function (_success2) {
+	        function success(_x2) {
+	          return _success2.apply(this, arguments);
+	        }
+	
+	        success.toString = function () {
+	          return _success2.toString();
+	        };
+	
+	        return success;
+	      }(function (data) {
+	        console.log("handling success");
+	        success();
+	      }),
+	      fail: function fail(data, code) {
+	        console.log("handling fail, code=" + code);
+	      }
+	    });
+	  },
+	  onCreate: function onCreate() {
+	    var _this = this;
+	
+	    console.info('Application onCreate');
+	    _system2.default.get({
+	      key: 'app_data',
+	      success: function success(data) {
+	        console.log("handling success");
+	        if (data) {
+	          _this.$def.data.items = JSON.parse(data);
+	        }
+	      },
+	      fail: function fail(data, code) {
+	        console.log("handling fail, code=" + code);
+	      }
+	    });
+	  },
+	  onDestroy: function onDestroy() {
+	    console.info('Application onDestroy');
+	  }
+	};
+	(exports.default || module.exports).manifest = {"package":"com.quick.notebook.plus","name":"快记事","versionName":"1.0.0","versionCode":"2","minPlatformVersion":"101","icon":"/Common/officialmark.png","features":[{"name":"system.prompt"},{"name":"system.router"},{"name":"system.shortcut"},{"name":"system.storage"}],"permissions":[{"origin":"*"}],"config":{"logLevel":"log"},"router":{"entry":"Home","pages":{"Home":{"component":"index"},"Edit":{"component":"index"},"About":{"component":"index"}}},"display":{"titleBarBackgroundColor":"#4d2517","titleBarTextColor":"#ad9a94","menu":true,"titleBarText":"快记事","pages":{"Home":{},"Edit":{},"About":{"menu":false}}}};
+	}
+
+/***/ },
+/* 11 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = { "default": __webpack_require__(12), __esModule: true };
+
+/***/ },
+/* 12 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var core = __webpack_require__(13);
+	var $JSON = core.JSON || (core.JSON = { stringify: JSON.stringify });
+	module.exports = function stringify(it) { // eslint-disable-line no-unused-vars
+	  return $JSON.stringify.apply($JSON, arguments);
+	};
+
+
+/***/ },
+/* 13 */
+/***/ function(module, exports) {
+
+	var core = module.exports = { version: '2.5.3' };
+	if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
+
+
+/***/ },
+/* 14 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * 显示菜单
+	 */
+	function showMenu() {
+	  var prompt = $app_require$('@app-module/system.prompt');
+	  var router = $app_require$('@app-module/system.router');
+	  var appInfo = $app_require$('@app-module/system.app').getInfo();
+	  prompt.showContextMenu({
+	    itemList: ['保存桌面', '关于', '取消'],
+	    success: function success(ret) {
+	      switch (ret.index) {
+	        case 0:
+	          // 保存桌面
+	          createShortcut();
+	          break;
+	        case 1:
+	          // 关于
+	          router.push({
+	            uri: '/About',
+	            params: {
+	              name: appInfo.name,
+	              icon: appInfo.icon
+	            }
+	          });
+	          break;
+	        case 2:
+	          // 取消
+	          break;
+	        default:
+	          prompt.showToast({
+	            message: 'error'
+	          });
+	      }
+	    }
+	  });
+	}
+	
+	/**
+	 * 创建桌面图标
+	 * 注意：使用加载器测试`创建桌面快捷方式`功能时，请先在`系统设置`中打开`应用加载器`的`桌面快捷方式`权限
+	 */
+	function createShortcut() {
+	  var prompt = $app_require$('@app-module/system.prompt');
+	  var shortcut = $app_require$('@app-module/system.shortcut');
+	  shortcut.hasInstalled({
+	    success: function success(ret) {
+	      if (ret) {
+	        prompt.showToast({
+	          message: '已创建桌面图标'
+	        });
+	      } else {
+	        shortcut.install({
+	          success: function success() {
+	            prompt.showToast({
+	              message: '成功创建桌面图标'
+	            });
+	          },
+	          fail: function fail(errmsg, errcode) {
+	            prompt.showToast({
+	              message: errcode + ': ' + errmsg
+	            });
+	          }
+	        });
+	      }
+	    }
+	  });
+	}
+	
+	exports.default = {
+	  showMenu: showMenu,
+	  createShortcut: createShortcut
+	};
+
+/***/ }
+/******/ ]);
+  };
+  if (typeof window === "undefined") {
+    return createAppHandler();
+  }
+  else {
+    window.createAppHandler = createAppHandler
+    // H5注入manifest以获取features
+    global.manifest = {"package":"com.quick.notebook.plus","name":"快记事","versionName":"1.0.0","versionCode":"2","minPlatformVersion":"101","icon":"/Common/officialmark.png","features":[{"name":"system.prompt"},{"name":"system.router"},{"name":"system.shortcut"},{"name":"system.storage"}],"permissions":[{"origin":"*"}],"config":{"logLevel":"log"},"router":{"entry":"Home","pages":{"Home":{"component":"index"},"Edit":{"component":"index"},"About":{"component":"index"}}},"display":{"titleBarBackgroundColor":"#4d2517","titleBarTextColor":"#ad9a94","menu":true,"titleBarText":"快记事","pages":{"Home":{},"Edit":{},"About":{"menu":false}}}};
+  }
+})();
+//# sourceMappingURL=app.js.map
